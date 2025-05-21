27c64e361f8c4aff5e2d68801445554d987d3133
celilensar <113338225+celilensar@users.noreply.github.com>
2025-05-21 23:17:45 +0300

    All designgs are handled.

--- TourismWebsite/Controllers/HomeController.cs
+++ TourismWebsite/Controllers/HomeController.cs
@@ -1,21 +1,31 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using TourismWebsite.Models;
+using TourismWebsite.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace TourismWebsite.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly TourismContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, TourismContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        var featuredDestinations = await _context.Destinations
+                                            .OrderBy(d => d.Id)
+                                            .Take(4)
+                                            .ToListAsync();
+        
+        return View(featuredDestinations);
     }
 
     public IActionResult Privacy()
--- TourismWebsite/obj/Debug/net9.0/TourismWebsite.AssemblyInfo.cs
+++ TourismWebsite/obj/Debug/net9.0/TourismWebsite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TourismWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3aa0acc13ee86041b946836511428e53e887f85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f55a5537731dbec9b264858be5a3b62a30edcd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TourismWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TourismWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
